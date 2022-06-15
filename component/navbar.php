50d9fcddd3f4d90a0973c98b22311ceb29a7fb04
Chandra Perdiansyah <66311673+cperdiansyah@users.noreply.github.com>
2022-06-15 23:43:08 +0700

    feat : fix our service

---
+++ component/navbar.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top ">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+            <img src="../asset/image/logo-putih.png" alt="kahta" width="100" height="30px">
+        </a>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="material.html">Material Product</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="order.html">Custom Order</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <form class="d-flex">
+                        <a class="nav-link me-3" href="login.html">Login</a>
+                        <button class="btn btn-light text-[#A25E91] bg-white " type="submit">Registrasi</button>
+                    </form>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
