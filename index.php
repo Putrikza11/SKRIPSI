ab0d62bc08bf6aa6d28e95ab6d421ea8de8ddcb1
Chandra Perdiansyah <66311673+cperdiansyah@users.noreply.github.com>
2022-06-15 21:14:32 +0700

    feat : initial project

---
+++ index.php
@@ -0,0 +1,263 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
+    <!-- CSS -->
+    <link rel="stylesheet" href="style.css">
+    <!-- LINK FONT -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600&display=swap" rel="stylesheet">
+    
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+
+    <title>Hello, world!</title>
+</head>
+
+<body>
+    <!-- NAVBAR -->
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top ">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="image/logo-putih.png" alt="kahta" width="auto" height="30px">
+            </a>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav mx-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="material.html">Material Product</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="order.html">Custom Order</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <form class="d-flex">
+                            <a class="nav-link me-3" href="login.html">Login</a>
+                            <button class="btn btn-light" style="color: #A25E91;" type="submit">Registrasi</button>
+                        </form>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- END NAVBAR -->
+
+    <!-- JUMBOTRON -->
+    <div class="container-fluid banner">
+        <div class="container h-100">
+
+            <div class="row h-100">
+                <div class="col-md-6 my-auto tagline">
+                    <h1 class="fw-semibold">BUAT PRODUKMU TERLIHAT EKSLUSIF</h1>
+                    <p class="fw-medium">
+                        Customize produkmu dengan Bahan, Ukuran, dan Desain yang
+                        kamu mau untuk meningkatkan citra produkmu dan menjadi jalan
+                        pintas untuk Branding.
+                    </p>
+                    <a href="registrasi.html" class="btn text-white" style="background-color:#D26ECF;" role="button" data-bs-toggle="button">DAFTAR SEKARANG</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- END JUMBOTRON -->
+
+    <!-- ABOUT US -->
+    <div class="container-fluid-about">
+        <div class="row features mt-5">
+            <div class="col-md-4 my-auto">
+                <h4>ABOUT US</h4>
+            </div>
+            <div class="col-md-8">
+                <P>"<strong>Kahtapack</strong>
+                    berdiri pada tahun 2017 dengan produk
+                    custom pertama yaitu blabla dan kemudian
+                    berkembang memberikan layanan jasa cetak untuk
+                    kemasan custom berbahan kertas. Produk dengan
+                    berbahan kertas menjadi bahan utama kami."
+                </P>
+            </div>
+        </div>
+    </div>
+    <!-- END ABOUT -->
+
+    <!-- OUR SERVICE -->
+    <div class="container">
+        <div class="row text-center mt-5">
+            <div class="title-service">
+                <h4>OUR SERVICE</h4>
+            </div>
+            <div class="content-service">
+                <p>Kami menyediakan apa yang kamu butuhkan</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="card-group">
+                <div class="col-md-3">
+                    <div class="card shadow p-3 mb-5">
+                        <img src="image/bahan.png" class="card-img-top" alt="...">
+                        <h5 class="title-card text-center">Bahan utama</h5>
+                        <div class="card-body">
+                            <p class="card-text text-center">
+                                Bahan utama yang digunakan terdiri dari 3 bahan yaitu Bahan
+                                Ivory, Bahan Duplex, dan Bahan
+                                Kraft. kamu dapat menyesuaikan
+                                bahan yang kamu inginkan.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card shadow p-3 mb-5">
+                        <img src="image/laminasi.png" class="card-img-top" alt="...">
+                        <h5 class="title-card text-center">Laminasi</h5>
+                        <div class="card-body">
+                            <p class="card-text text-center">Membuat tampilan kertas
+                                menjadi lebih menarik dengan
+                                menggunakan laminasi glossy
+                                dan laminasi doff di sisi luar
+                                maupun dalam.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card shadow p-3 mb-5">
+                        <img src="image/model.png" class="card-img-top" alt="...">
+                        <h5 class="title-card text-center">Model Kemasan</h5>
+                        <div class="card-body">
+                            <p class="card-text text-center">Berbagai bentuk yang kamu ingin
+                                dan sesuaikan dengan kebutuhan
+                                mulai dari Model sambung
+                                ataupun Model terpisah antara
+                                penutup dan wadah.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card shadow p-3 mb-5">
+                        <img src="image/gambar.png" class="card-img-top" alt="...">
+                        <h5 class="title-card text-center">Contoh Produk</h5>
+                        <div class="card-body">
+                            <p class="card-text text-center">Beberapa contoh produk yang di custom
+                                oleh customer dengan spesifikasi yang bisa kamu jadikan
+                                inspirasi dan modifikasi kembali sesuai kebutuhanmu.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row text-center link-service">
+            <a href="">
+                <h5>View More <i class="bi bi-chevron-compact-right"></i></h5>
+
+            </a>
+        </div>
+    </div>
+    <!-- END SERVICE -->
+
+    <!-- ORDER -->
+    <div class="container">
+        <div class="row mt-5">
+            <div class="title-order text-center">
+                <h4>HOW TO ORDER?</h4>
+            </div>
+        </div>
+        <div class="row content-order text-center">
+            <div class="col ">
+                <img src="image/form.png" class="card-img-top" alt="...">
+            </div>
+            <div class="col">
+                <img src="image/chat.png" class="card-img-top" alt="...">
+            </div>
+            <div class="col">
+                <img src="image/buy.png" class="card-img-top" alt="...">
+            </div>
+            <div class="col">
+                <img src="image/produksi.png" class="card-img-top" alt="...">
+            </div>
+            <div class="col">
+                <img src="image/shipping.png" class="card-img-top" alt="...">
+            </div>
+        </div>
+        <div class="row content-order text-center">
+            <div class="col ">
+                <h6>Mengisi form sesuai pesanan</h6>
+            </div>
+            <div class="col">
+                <h6>Konsultasi Desain</h6>
+            </div>
+            <div class="col">
+                <h6>Melakukan Pembayaran Uang Muka</h6>
+            </div>
+            <div class="col">
+                <h6>Proses Produksi</h6>
+            </div>
+            <div class="col">
+                <h6>Produk siap dikirim</h6>
+            </div>
+        </div>
+
+    </div>
+    <!-- END ORDER -->
+
+    <!-- CTA -->
+    <div class="container">
+        <div class="row CTA mt-5 text-center">
+            <h5 class="content-CTA px-5">Siap untuk mewujudkan kemasan yang sesuai
+                dengan keinginan dan kebutuhan kamu? </h5>
+
+            <div class="col text-center">
+                <a href="">
+                    <button class="btn btn-CTA btn-light fs-4 border border-2 border-secondary" style="border-radius: 20px; font-weight: bold;" type="submit">Cek Harga</button>
+                </a>
+            </div>
+
+        </div>
+    </div>
+    <!-- END CTA -->
+    <hr>
+    <!-- FOOTER -->
+    <div class="container footer">
+
+        <div class="row mx-4">
+            <div class="col">
+                <img src="image/logo-ungu.png" alt="" width="50px">
+                <div class="row">
+                    <p>Jl. Kebayoran Lama No.17, Jakarta selatan</p>
+                </div>
+            </div>
+            <div class="col text-center">
+                <p>About us</p>
+            </div>
+            <div class="col text-center">
+                <p>Product</p>
+            </div>
+            <div class="col">
+                <p>custom.kahta@gmail.com</p>
+                <a href=""><i class="bi bi-instagram"></i></a>
+                <a href=""><i class="bi bi-facebook"></i></a>
+                <a href=""><i class="bi bi-whatsapp"></i></a>
+
+            </div>
+        </div>
+    </div>
+    <!-- END FOOTER -->
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+
+</body>
+
+</html>
